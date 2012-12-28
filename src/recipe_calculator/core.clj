3e1c8571d652f14147dc4dd10f5dd66f2b4025c2
Luchs <Luchs@euirc.eu>
2012-12-28 01:33:18 +0100

    Fix stock content typo

--- src/recipe_calculator/core.clj
+++ src/recipe_calculator/core.clj
@@ -9,7 +9,7 @@
                :zucker 9000
                :mehl 18000
                :eier 195
-               :mandeln 775}
+               :mandeln 7750}
         solutions (solver/solve stock)]
     (doseq [solution solutions]
       (println solution))))
